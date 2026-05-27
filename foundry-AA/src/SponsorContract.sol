3713536e7c931dff5a1e56b4a56f90f843499f72
Contzo <ilierazvan1497@pm.me>
2026-05-27 15:56:24 +0300

    Added address prediction

---
+++ foundry-AA/src/SponsorContract.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IPaymaster, PackedUserOperation} from "account-abstraction/interfaces/IPaymaster.sol";
+
+contract SponsorContract {}
+
